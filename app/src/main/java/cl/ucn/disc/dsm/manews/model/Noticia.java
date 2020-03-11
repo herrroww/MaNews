f55cf590ac54a5501f133feb7d7b75ecd12ea79d
herrroww <mob010@alumnos.ucn.cl>
2020-03-10 23:58:54 -0300

    feat:add Noticia.java and NoticiaTest.java

---
+++ app/src/main/java/cl/ucn/disc/dsm/manews/model/Noticia.java
@@ -0,0 +1,175 @@
+/* Copyright [2020] [Martin Osorio]
+    *
+    * Licensed under the Apache License, Version 2.0 (the "License");
+    * you may not use this file except in compliance with the License.
+    * You may obtain a copy of the License at
+    *
+    *     http://www.apache.org/licenses/LICENSE-2.0
+    *
+    * Unless required by applicable law or agreed to in writing, software
+    * distributed under the License is distributed on an "AS IS" BASIS,
+    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    * See the License for the specific language governing permissions and
+    * limitations under the License.
+    */
+
+package cl.ucn.disc.dsm.manews.model;
+
+import org.threeten.bp.ZoneId;
+import org.threeten.bp.ZonedDateTime;
+
+/**
+ * Domain class.
+ */
+public final class Noticia {
+
+  /**
+   * The Zone.
+   */
+  public static final ZoneId ZONE_ID = ZoneId.of("-3");
+
+  /**
+   * The Id.
+   */
+  public final Long id;
+
+  /**
+   * The Titulo.
+   */
+  public final String titulo;
+
+  /**
+   * The Fuente.
+   */
+  public final String fuente;
+
+  /**
+   * The Author.
+   */
+  public final String autor;
+
+  /**
+   * The URL.
+   */
+  public final String url;
+
+  /**
+   * The URL of Photo.
+   */
+  public final String urlFoto;
+
+  /**
+   * The Resumen.
+   */
+  public final String resumen;
+
+  /**
+   * The Contenido.
+   */
+  public final String contenido;
+
+  /**
+   * The Fecha.
+   */
+  public final ZonedDateTime fecha;
+
+  /**
+   * The Constructor.
+   *
+   * @param id        , id de la noticia
+   * @param titulo    , titulo de la noticia
+   * @param fuente    , fuente de la noticia
+   * @param autor     , autor de la noticia
+   * @param url       , url de la noticia
+   * @param urlFoto   , url de la foto que tiene la noticia
+   * @param resumen   , resumen de la noticia
+   * @param contenido , contenido de la noticia
+   * @param fecha     , fecha de publicacion de la noticia
+   */
+  public Noticia(Long id, String titulo, String fuente, String autor,
+      String url, String urlFoto, String resumen, String contenido,
+      ZonedDateTime fecha) {
+    this.id = id;
+    this.titulo = titulo;
+    this.fuente = fuente;
+    this.autor = autor;
+    this.url = url;
+    this.urlFoto = urlFoto;
+    this.resumen = resumen;
+    this.contenido = contenido;
+    this.fecha = fecha;
+  }
+
+  /**
+   * The getID.
+   * @return The id.
+   */
+  public Long getId() {
+    return this.id;
+  }
+
+  /**
+   * Obtener el itulo de la noticia.
+   * @return The titulo.
+   */
+  public String getTitulo() {
+    return this.titulo;
+  }
+
+  /**
+   * Obtener la fuente de la noticia.
+   * @return The fuente.
+   */
+  public String getFuente() {
+    return this.fuente;
+  }
+
+  /**
+   * Obtener el autor de la noticia.
+   * @return The autor.
+   */
+  public String getAutor() {
+    return this.autor;
+  }
+
+  /**
+   * Obtener la url de la noticia.
+   * @return The url.
+   */
+  public String getUrl() {
+    return this.url;
+  }
+
+  /**
+   * Obtener la url de la Foto.
+   * @return The urlFoto.
+   */
+  public String getUrlFoto() {
+    return this.urlFoto;
+  }
+
+  /**
+   * Obtener el resumen de la noticia.
+   * @return The resumen.
+   */
+  public String getResumen() {
+    return this.resumen;
+  }
+
+  /**
+   * Obtener el contenido de la noticia.
+   * @return The contenido.
+   */
+  public String getContenido() {
+    return this.contenido;
+  }
+
+  /**
+   * Obtener la fecha de la noticia.
+   * @return The fecha.
+   */
+  public ZonedDateTime getFecha() {
+    return this.fecha;
+  }
+
+}
---
+++ app/src/test/java/cl/ucn/disc/dsm/manews/model/NoticiaTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2019-2020 Martin Osorio Bugueño <mob010@alumnos.ucn.cl>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cl.ucn.disc.dsm.manews.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.threeten.bp.ZonedDateTime;
+
+/**
+ * Test of {@link cl.ucn.disc.dsm.manews.model.Noticia}.
+ *
+ * @author Martin Osorio Bugueño.
+ */
+public class NoticiaTest {
+
+  /**
+   * The Logger.
+   */
+  private static final Logger log = LoggerFactory.getLogger(NoticiaTest.class);
+
+  /**
+   * The Test of Constructor.
+   */
+  @Test
+  public void testConstructor() {
+
+    log.debug("Testing the Constructor ..");
+
+    // The values
+    final Long id = 1L;
+    final String titulo = "The Titulo";
+    final String fuente = "The Fuente";
+    final String autor = "The Author";
+    final String url = "The URL";
+    final String urlFoto = "The URL of Foto";
+    final String resumen = "The Resumen";
+    final String contenido = "The Contenido";
+    final ZonedDateTime fecha = ZonedDateTime.now(Noticia.ZONE_ID);
+
+    // The Constructor
+    final Noticia noticia = new Noticia(id, titulo, fuente, autor, url, urlFoto, resumen, contenido, fecha);
+
+    // Testing
+    Assertions.assertEquals(id, noticia.getId());
+    Assertions.assertEquals(titulo, noticia.getTitulo());
+    Assertions.assertEquals(fuente, noticia.getFuente());
+    Assertions.assertEquals(autor, noticia.getAutor());
+    Assertions.assertEquals(url, noticia.getUrl());
+    Assertions.assertEquals(urlFoto, noticia.getUrlFoto());
+    Assertions.assertEquals(resumen, noticia.getResumen());
+    Assertions.assertEquals(contenido, noticia.getContenido());
+    Assertions.assertEquals(fecha, noticia.getFecha());
+
+    log.debug("Done.");
+
+  }
+
+}
