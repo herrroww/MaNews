26efc11f83edadb01de35e2663cb7fecee218cf8
herrroww <mob010@alumnos.ucn.cl>
2020-03-11 16:53:14 -0300

    feat(adapters): NoticiaAdapter.java

---
+++ app/src/main/java/cl/ucn/disc/dsm/manews/activities/adapters/NoticiaAdapter.java
@@ -0,0 +1,89 @@
+/* Copyright [2020] [Martin Osorio]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cl.ucn.disc.dsm.manews.activities.adapters;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import cl.ucn.disc.dsm.manews.activities.adapters.NoticiaViewHolder;
+import cl.ucn.disc.dsm.manews.databinding.RowNoticiaBinding;
+import cl.ucn.disc.dsm.manews.model.Noticia;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ViewHolder Pattern.
+ *
+ * @author Martin Osorio Bugueño.
+ */
+public class NoticiaAdapter extends RecyclerView.Adapter<NoticiaViewHolder> {
+
+  /**
+   * The List of Noticias.
+   */
+  private List<Noticia> theNoticias;
+
+  /**
+   * The Constructor.
+   */
+  public NoticiaAdapter() {
+    this.theNoticias = new ArrayList<>();
+  }
+
+  /**
+   * Change the current List of Noticias.
+   *
+   * @param noticias to use.
+   */
+  public void setNoticias(final List<Noticia> noticias) {
+
+    // Update the noticias
+    this.theNoticias = noticias;
+
+    // Notify to re-layout
+    this.notifyDataSetChanged();
+  }
+
+  /**
+   * Called when RecyclerView needs a newViewHolder of the given type to represent an item.
+   */
+  @Override
+  public NoticiaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+    final LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+    return new NoticiaViewHolder(RowNoticiaBinding.inflate(layoutInflater, parent, false));
+
+  }
+
+  /**
+   * Called by RecyclerView to display the data at the specified position. This method should update the contents of the
+   * ViewHolder to reflect the item at the given position.
+   */
+  @Override
+  public void onBindViewHolder(@NonNull NoticiaViewHolder holder, int position) {
+    holder.bind(this.theNoticias.get(position));
+  }
+
+  /**
+   * Returns the total number of items in the data set held by the adapter.
+   */
+  @Override
+  public int getItemCount() {
+    return this.theNoticias.size();
+  }
+
+}
