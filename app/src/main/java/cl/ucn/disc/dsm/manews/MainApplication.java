5f18e759fc3054907c9b8c481c6161cf1fb78468
herrroww <mob010@alumnos.ucn.cl>
2020-03-13 01:45:03 -0300

    chore(): Configuration strictmode

--- app/src/main/java/cl/ucn/disc/dsm/manews/MainApplication.java
+++ app/src/main/java/cl/ucn/disc/dsm/manews/MainApplication.java
@@ -1,8 +1,31 @@
+/* Copyright [2020] [Martin Osorio]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cl.ucn.disc.dsm.manews;
 
 import android.app.Application;
+import android.os.Build.VERSION_CODES;
+import android.os.StrictMode;
+import android.os.StrictMode.ThreadPolicy;
+import android.os.StrictMode.VmPolicy;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatDelegate;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.facebook.imagepipeline.decoder.SimpleProgressiveJpegConfig;
+import com.jakewharton.threetenabp.AndroidThreeTen;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,21 +45,57 @@ public class MainApplication extends Application {
    * Called when the application is starting, before any activity, service, or receiver objects (excluding content
    * providers) have been created.
    */
+  @RequiresApi(api = VERSION_CODES.M)
   @Override
   public void onCreate() {
     super.onCreate();
 
+    log.debug("Initializing ..");
+
+
+      // Here we write the code in debug build.
 
 
     // Day and Night support
     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
 
-    log.debug("Initializing: Done.");
+      // Fresco configuration for large images
 
-    // Facebook fresco
+      ImagePipelineConfig config = ImagePipelineConfig.newBuilder(this)
+          .setProgressiveJpegConfig(new SimpleProgressiveJpegConfig())
+          .setResizeAndRotateEnabledForNetwork(true)
+          .setDownsampleEnabled(true)
+          .build();
+
+
+      // Facebook fresco
     Fresco.initialize(this);
 
-    log.debug("Initializing: Done.");
+      // AndroidThreeTen initialization
+      AndroidThreeTen.init(this);
+
+      if (BuildConfig.DEBUG) {
+
+        StrictMode.setThreadPolicy(new ThreadPolicy.Builder()
+            .detectDiskReads()
+            .detectDiskWrites()
+            .detectNetwork()
+            .penaltyLog()
+            .penaltyFlashScreen()
+            .build());
+
+        StrictMode.setVmPolicy(new VmPolicy.Builder()
+            .detectLeakedSqlLiteObjects()
+            .detectLeakedClosableObjects()
+            .detectLeakedRegistrationObjects()
+            .detectActivityLeaks()
+            .detectCleartextNetwork()
+            .penaltyLog()
+            .build());
+
+      }
+
+      log.debug("Initializing: Done.");
+    }
   }
 
-}
